d2c6557bca5b24b5ac907c956e4c16c6d133f4c3
Mikhail Matrosov <mikhail.matrosov@gmail.com>
2017-04-06 22:36:18 +0300

    (=) Reformat according to PEP8 and to make PyCharm (mostly) happy

--- DllDispatcher.py
+++ DllDispatcher.py
@@ -9,36 +9,38 @@ IMAGE_FILE_MACHINE_I386 = 332
 IMAGE_FILE_MACHINE_IA64 = 512
 IMAGE_FILE_MACHINE_AMD64 = 34404
 
+
 # Source: http://stackoverflow.com/a/1345697/261217
-def ReadPEMachineType(filepath):
-  with open(filepath, "rb") as f:
-    s = f.read(2)
-    if s.decode() != 'MZ':
-      raise OSError("File not recognized as PE.")
+def read_pe_machine_type(file_path):
+    with open(file_path, "rb") as f:
+        s = f.read(2)
+        if s.decode() != 'MZ':
+            raise OSError("File not recognized as PE.")
 
-    f.seek(60)
-    s = f.read(4)
-    header_offset = struct.unpack("<L", s)[0]
-    f.seek(header_offset + 4)
-    s = f.read(2)
-    return struct.unpack("<H", s)[0]
+        f.seek(60)
+        s = f.read(4)
+        header_offset = struct.unpack("<L", s)[0]
+        f.seek(header_offset + 4)
+        s = f.read(2)
+        return struct.unpack("<H", s)[0]
 
-def Is64bitPE(filepath):
-  machine = ReadPEMachineType(filepath)
 
-  if machine == IMAGE_FILE_MACHINE_I386:
-    return False
-  elif machine in [IMAGE_FILE_MACHINE_IA64, IMAGE_FILE_MACHINE_AMD64]:
+def is_64bit_pe(file_path):
+    machine = read_pe_machine_type(file_path)
+
+    if machine == IMAGE_FILE_MACHINE_I386:
+        return False
+    elif machine in [IMAGE_FILE_MACHINE_IA64, IMAGE_FILE_MACHINE_AMD64]:
+        return True
+    else:
+        print("Unknown file architecture. Defaulted to 64-bit application.")
+
     return True
-  else:
-    print("Unknown file architecture. Defaulted to 64-bit application.")
-
-  return True
 
 
-filepath = sys.argv[1]
-app = win64_app if Is64bitPE(filepath) else win32_app
+file_path = sys.argv[1]
+app = win64_app if is_64bit_pe(file_path) else win32_app
 
 print("Starting {}...".format(app))
-args = [app, filepath];
+args = [app, file_path]
 subprocess.Popen(args)
